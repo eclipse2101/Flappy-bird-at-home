e01f17871a72e9036e876b0679d5f58a701e1a8e
eclipse2101 <rdericieux25@aoiths.org>
2023-04-20 08:29:34 -0400

    e

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    Rigidbody2D flappy;
+      
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flappy = GetComponent<Rigidbody2D>(); // this gets the 2d rigidbody from an object
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          flappy.AddForce(new Vector2(0,1) * 200);
+        }
+    }
+}
