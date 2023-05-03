44ca3851d7f603d869653586ae608b5f290bfe9b
eclipse2101 <rdericieux25@aoiths.org>
2023-05-03 09:05:35 -0400

    my ver impossible game is now done to an extent

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class BirdScript : MonoBehaviour
 {
     Rigidbody2D flappy;
-    float horizontal;
-   public bool playerIsStillPlaying; // this variable checks if th layer touched a pipe or not. If the player does a game over screen will pop up and no more pipes will spawn 
-      
+     float horizontal;
+     public bool playerIsStillPlaying; // this variable checks if th layer touched a pipe or not. If the player does a game over screen will pop up and no more pipes will spawn 
+      int score = 0;
+      public Text scoreUI; 
 
     // Start is called before the first frame update
     void Start()
@@ -32,7 +35,19 @@ public class BirdScript : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
+        
+        
         // this cause the the playerIsStillPlaying script to become false and make te pipes stop spawning and the game over screen will pop up
         playerIsStillPlaying = false;
     }    
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Points"))
+                {
+                    score = score + 1; 
+                    // this will increase the score by 1 if the bird collides with a tag called Points
+                    scoreUI.text = score.ToString();
+                }
+    }
 }
--- Assets/Scripts/ScoreKeeperScript.cs
+++ Assets/Scripts/ScoreKeeperScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ScoreKeeperScript : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
